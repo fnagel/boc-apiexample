00f8d2674ca3f3eb65c106fde6c36af1a3cf1b6f
Felix Nagel <info@felixnagel.com>
2014-07-24 15:24:44 +0200

    [TASK] Init repo with v0.0.3

---
+++ README.txt
@@ -0,0 +1,4 @@
+This is a minimal example of the Beauty Of Code TYPO3 extension API.
+
+Please take a look in the documentation linked here:
+http://typo3.org/extensions/repository/view/beautyofcode/current/
---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "boc_apiexample".
+ *
+ * Auto generated 11-03-2013 21:39
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'BeautyOfCode API Example',
+	'description' => 'This is an API example of EXT: beautyofcode',
+	'category' => 'example',
+	'author' => 'Felix Nagel',
+	'author_email' => 'info@felixnagel.com',
+	'shy' => '',
+	'dependencies' => 'beautyofcode',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.3',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.3.0-6.0.99',
+			'beautyofcode' => '0.8.0-1.0.3',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"7b06";s:12:"ext_icon.gif";s:4:"1706";s:17:"ext_localconf.php";s:4:"5461";s:14:"ext_tables.php";s:4:"b5fb";s:16:"locallang_db.xml";s:4:"13b6";s:10:"README.txt";s:4:"da45";s:34:"pi1/class.tx_bocapiexample_pi1.php";s:4:"05c0";s:17:"pi1/locallang.xml";s:4:"042f";}',
+	'suggests' => array(
+	),
+);
+
+?>
