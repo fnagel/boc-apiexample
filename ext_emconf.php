94142f4eca0a97ef748bfb8c652864cb2423cbd9
Thomas Juhnke <tommy@van-tomas.de>
2014-08-11 13:40:24 +0200

    [TASK] Updating version constraints in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,15 +32,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.3',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-6.0.99',
-			'beautyofcode' => '0.8.0-1.0.3',
+			'typo3' => '6.2.0-6.2.99',
+			'beautyofcode' => '2.0.0-2.0.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"7b06";s:12:"ext_icon.gif";s:4:"1706";s:17:"ext_localconf.php";s:4:"5461";s:14:"ext_tables.php";s:4:"b5fb";s:16:"locallang_db.xml";s:4:"13b6";s:10:"README.txt";s:4:"da45";s:34:"pi1/class.tx_bocapiexample_pi1.php";s:4:"05c0";s:17:"pi1/locallang.xml";s:4:"042f";}',
 	'suggests' => array(
 	),
 );
