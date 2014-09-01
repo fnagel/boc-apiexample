773cca59fa479f831476b2a2fe5d43b4aa688c94
Felix Nagel <info@felixnagel.com>
2014-09-01 17:42:35 +0200

    [TASK] Update copyright

--- Classes/Controller/ApiController.php
+++ Classes/Controller/ApiController.php
@@ -4,7 +4,7 @@ namespace TYPO3\BocApiexample\Controller;
 /***************************************************************
  * Copyright notice
  *
- * (c) 2011 Felix Nagel <info@felixnagel.com>
+ * (c) 2014 Thomas Juhnke <typo3@van-tomas.de>
  * All rights reserved
  *
  * This script is part of the TYPO3 project. The TYPO3 project is
